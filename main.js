46512912419cde91e6621ebba6a9f6ebbca077d5
Cantrip Forge <cantripforge@gmail.com>
2018-03-18 18:53:44 -0400

    working loop through stories

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const os       = require("os");
 const setup    = require("./setup");
-const readline = require("readline");
+const readline = require("readline-sync");
 
 function cleanInput(bytes){
   return bytes.toString().split(os.EOL).filter( input => input != '');
@@ -10,19 +10,21 @@ const stories  = cleanInput(setup.getStories());
 const players  = cleanInput(setup.getPlayers());
 const stations = cleanInput(setup.getStations());
 
-const rl = readline.createInterface({
-  input:  process.stdin,
-  output: process.stdout
-});
+// set up "stories" object to collect player scores
+storiesScoreMap = stories.map( story => [story, [] ]);
 
-for(i = 0; i<stories.length; i++){
+for(i = 0; i<1 /* stories.length */; i++){
 
   story = stories[i];
   storyScores = [story];
 
+  console.log(os.EOL + "Collecting scores for the following story: " + os.EOL + story + os.EOL);
+
   for(x = 0; x<players.length; x++){
     player = players[x];
-    rl.question('sup?!', (answer) => { console.log(answer + "!!") });
-    console.log(player);
+    score  = readline.question(`${player}, what do you score this story? `);
+    storiesScoreMap[i][1].push( { player: player, score: score } );
   }
 }
+
+console.log(storiesScoreMap);
